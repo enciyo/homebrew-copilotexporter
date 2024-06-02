d84612dd7091348709d1c5d532c8120dbd11bdd1
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-02 21:07:28 +0300

    fix

--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -3,14 +3,13 @@ class Copilothistoryexporter < Formula
 
   desc "This package exports the history of GitHub Copilot chat history."
   homepage "https://github.com/enciyo/gh-copilot-history-export"
-  url "https://files.pythonhosted.org/packages/a8/59/be3ec882235e834d8ffe9ced57edd7f4a7279ac85426d3697d580a1bc179/copilothistoryexporter-1.0.2.1.tar.gz"
-  sha256 "b06afc4f605c5a429c2c05b754aae81a9a6f7bf6b02e8dcc8253dfbc2a3fb93a"
-
+  url "h9d6a73640b125c19eb6b6d1e409aea19eba70b554e7518557a3ecd78d3d8a104"
+  sha256 "9d6a73640b125c19eb6b6d1e409aea19eba70b554e7518557a3ecd78d3d8a104"
   depends_on "python@3.9"  # Adjust if necessary
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/a8/59/be3ec882235e834d8ffe9ced57edd7f4a7279ac85426d3697d580a1bc179/copilothistoryexporter-1.0.2.1.tar.gz"
-    sha256 "b06afc4f605c5a429c2c05b754aae81a9a6f7bf6b02e8dcc8253dfbc2a3fb93a"
+    url "h9d6a73640b125c19eb6b6d1e409aea19eba70b554e7518557a3ecd78d3d8a104"
+    sha256 "9d6a73640b125c19eb6b6d1e409aea19eba70b554e7518557a3ecd78d3d8a104"
   end
 
   def install
