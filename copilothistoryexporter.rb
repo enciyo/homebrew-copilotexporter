14502c0bcbb99b48801e572ad9a082b1cf5f945a
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-02 21:30:12 +0300

    fix

--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -5,7 +5,7 @@ class Copilothistoryexporter < Formula
   homepage "https://github.com/enciyo/gh-copilot-history-export"
   url "https://files.pythonhosted.org/packages/51/45/b1968f2dea6d9a4fdea861603c4e6b78faffe6f5a76656e80284d77419c5/copilothistoryexporter-1.0.2.4.tar.gz"
   sha256 "0f7afa2313fd00a143b7ea18a7b83137d55d436ae40da2f68638ec75134de88a"
-  depends_on "python@3.9.6"  # Adjust if necessary
+  depends_on "python@3.9"  # Adjust if necessary
 
 
 
