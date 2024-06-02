5b16768addb4d7189537f2357b926ae0350f20a1
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-02 20:43:50 +0300

    fix

--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -3,17 +3,17 @@ class Copilothistoryexporter < Formula
 
   desc "This package exports the history of GitHub Copilot chat history."
   homepage "https://github.com/enciyo/gh-copilot-history-export"
-  url "https://github.com/enciyo/gh-copilot-history-export/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "c2681c76b3c962b0a8b465713e5db6ac223384d3e2b665c10fef53edc1acdb84"
+
 
   depends_on "python@3.9"  # Adjust if necessary
 
+  resource "six" do
+    url "https://files.pythonhosted.org/packages/a8/59/be3ec882235e834d8ffe9ced57edd7f4a7279ac85426d3697d580a1bc179/copilothistoryexporter-1.0.2.1.tar.gz"
+    sha256 "b06afc4f605c5a429c2c05b754aae81a9a6f7bf6b02e8dcc8253dfbc2a3fb93a"
+  end
+
   def install
-      # Create a virtualenv in `libexec`.
-      venv = virtualenv_create(libexec, "python3.y")
-      venv.pip_install resources
-      venv.pip_install .
-      venv.pip_install_and_link buildpath
+    virtualenv_install_with_resources
   end
 
   test do
