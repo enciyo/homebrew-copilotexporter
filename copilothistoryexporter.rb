73c4c4033cdd75989278846cf61c114ea5ba76a5
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-02 19:54:41 +0300

    Refactored

--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -3,8 +3,8 @@ class Copilothistoryexporter < Formula
 
   desc "This package exports the history of GitHub Copilot chat history."
   homepage "https://github.com/enciyo/gh-copilot-history-export"
-  url "https://github.com/enciyo/gh-copilot-history-export/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "80f5757823d8076bb2c15e192e946a6eac373654ccd8d508de359eeb46e6ec81"
+  url "https://files.pythonhosted.org/packages/87/2c/f426e75662e1ab8951f1d80a2ce4d6359c61e64f46a95dd62635833c6536/copilothistoryexporter-1.0.2.tar.gz"
+  sha256 "1c58de2d9ff18835f020fde0b4a06891ddbac9c0d7a8f2331a63232eeec40e7f"
 
   depends_on "python@3.9"  # Adjust if necessary
 
