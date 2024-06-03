831f9681b5e4da39d0cdca682a66659a4df6b077
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-03 18:22:48 +0300

    reorganized

--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -10,6 +10,10 @@ class Copilothistoryexporter < Formula
     url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
   end
 
+  resource "mitmproxy" do
+    url "https://downloads.mitmproxy.org/10.3.0/mitmproxy-10.3.0.tar.gz"
+  end
+
   def install
     virtualenv_install_with_resources
   end
