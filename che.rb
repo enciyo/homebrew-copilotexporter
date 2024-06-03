6c0de660ec002e106a75951bcafa9af58e12243d
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-03 18:16:25 +0300

    reorganized

--- che.rb
+++ che.rb
@@ -1,11 +1,32 @@
 cask "che" do
-  version "1.0.1"
-  name "Safe"
-  desc "Safe"
-  homepage "https://github.com/tonsky/AnyBar"
-  depends_on formula: "python@3.12"
-  depends_on cask: "mitmproxy"
+  arch arm: "arm64", intel: "x86_64"
+
+  version "10.3.0"
+  sha256 arm:   "52ae3bb018d694ff0a0291bf5fc6958655dba1294cf18b21db3d35e383d87c79",
+         intel: "cb0b6d3ee8c6c566021be2220b71c890a1dd5216610b3689e6d1bcd70676c872"
+
+  url "https://downloads.mitmproxy.org/#{version}/mitmproxy-#{version}-macos-#{arch}.tar.gz"
+  name "mitmproxy"
+  desc "Intercept, modify, replay, save HTTP/S traffic"
+  homepage "https://mitmproxy.org/"
+
   depends_on formula: "copilothistoryexporter"
 
 
+  livecheck do
+    url "https://downloads.mitmproxy.org/list"
+    strategy :xml do |xml|
+      xml.get_elements("//Prefix").map do |item|
+        item.text&.strip&.delete_suffix("/")
+      end
+    end
+  end
+
+  binary "mitmproxy.app/Contents/MacOS/mitmproxy"
+  binary "mitmproxy.app/Contents/MacOS/mitmdump"
+  binary "mitmproxy.app/Contents/MacOS/mitmweb"
+
+
+
+  zap trash: "~/.mitmproxy"
 end
--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -4,8 +4,11 @@ class Copilothistoryexporter < Formula
   desc "This package exports the history of GitHub Copilot chat history."
   homepage "https://github.com/enciyo/gh-copilot-history-export"
   url "https://files.pythonhosted.org/packages/91/e2/c4e3ce6619afa6a78ce41f5272d60f45e1aff4f5cad0b6ec3f41aaa8366d/copilothistoryexporter-1.2.6.tar.gz"
+  depends_on "python@3.12"
 
-  depends_on "python@3.11"  # Adjust if necessary
+  resource "click" do
+    url "https://files.pythonhosted.org/packages/96/d3/f04c7bfcf5c1862a2a5b845c6b2b360488cf47af55dfa79c98f6a6bf98b5/click-8.1.7.tar.gz"
+  end
 
   def install
     virtualenv_install_with_resources
