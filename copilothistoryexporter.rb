f44828ca83f796378a81acae4ba941f5774d4fec
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-03 17:43:24 +0300

    Added cask

--- copilothistoryexporter.rb
+++ copilothistoryexporter.rb
@@ -6,16 +6,12 @@ class Copilothistoryexporter < Formula
   url "https://files.pythonhosted.org/packages/c6/6d/0d4dcb726b6d0e2c654baa115d310f8cbbef9e97be44698ad222f51229f4/copilothistoryexporter-1.2.4.tar.gz"
 
   depends_on "python@3.12"  # Adjust if necessary
-  depends_on "mitmproxy"
-
 
   def install
     venv = virtualenv_create(libexec, "python3")
     venv.pip_install resources
     venv.pip_install_and_link buildpath
-
   end
-
 
   test do
     system bin/"copilothistoryexporter", "--help"
