481c462e11ccec92c2462249ec46fb8f89786c07
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-07 18:37:47 +0300

    fix

--- gh-che.rb
+++ gh-che.rb
@@ -5,6 +5,7 @@ class GhChe < Formula
   desc "This package exports the history of GitHub Copilot chat history."
   version "1.0.3"
   url "https://github.com/enciyo/gh-che/archive/refs/heads/main.zip"
+  sha256 "ff6ed787298dd406f3e3ceab429a53e273b90ac05ed0c8a3b4eeb6fcd3371d0d"
 
   depends_on "python@3.11" => :build
 
