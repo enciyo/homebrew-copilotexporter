ee4861645c9b4a594c99df4bbcb579d4c5bc299a
enciyo <mustafa.kilic@ciceksepeti.com>
2024-06-08 15:27:50 +0300

    fix

--- gh-che.rb
+++ gh-che.rb
@@ -1,10 +1,16 @@
 class GhChe < Formula
   include Language::Python::Virtualenv
 
+  # read from the version git spesific repository
+  raw_url = "https://raw.githubusercontent.com/enciyo/gh-che/main/che.properties"
+  sh = `curl -s #{raw_url}`
+  version = sh.split("\n").select { |line| line.include? "version" }[0].split("=")[1]
+
+
   homepage "https://github.com/enciyo/gh-che"
   desc "This package exports the history of GitHub Copilot chat history."
-  version "1.0.8"
-  url "https://github.com/enciyo/gh-che/archive/refs/tags/v#{version}.tar.gz"
+  version version
+  url "https://github.com/enciyo/gh-che/archive/refs/tags/#{version}.tar.gz"
   depends_on "python@3.11" => :build
 
   def install
